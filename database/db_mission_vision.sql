d8951cac1e15689d35bc145108b8f3407872bc40
fernandezjerome <97711003+fernandezjerome@users.noreply.github.com>
2023-03-25 10:52:10 -0400

    aa

---
+++ database/db_mission_vision.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 16, 2023 at 01:59 AM
+-- Server version: 5.7.24
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_mission_vision`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basketballs`
+--
+
+CREATE TABLE `basketballs` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `section` varchar(50) NOT NULL,
+  `img` varchar(50) NOT NULL,
+  `alt` varchar(50) NOT NULL,
+  `info` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `basketballs`
+--
+
+INSERT INTO `basketballs` (`id`, `section`, `img`, `alt`, `info`) VALUES
+(1, 'who', 'group-shot.jpg', 'group-shot', 'With the belief that sports should be a place where everyone feels valued, respected, and encouraged to reach their full potential, we have come together to launch a movement against bullying in basketballs.'),
+(2, 'mission', 'mission.jpg', 'mission', 'Our mission to empower the basketballs community to rise above bullying through respect, kindness, and inclusiveness. Creating a safe and supportive environment for all to reach their full potential.'),
+(3, 'vision', 'vission.jpg', 'vission', 'Our vision is to transform basketballs into a safe, inclusive, and empowering place for all. Inspiring everyone to reach their full potential and find joy in the game.');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `basketballs`
+--
+ALTER TABLE `basketballs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `basketballs`
+--
+ALTER TABLE `basketballs`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
